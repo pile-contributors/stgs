cfe72eeb4afe9dc06d3e928dde088045dbaf4fa6
Nicu Tofan <nicu.tofan@gmail.com>
2015-11-23 17:33:38 +0200

    Using the same interface as PerSt

--- stgs.cc
+++ stgs.cc
@@ -9,20 +9,26 @@
 
 #include "stgs.h"
 #include "stgs-private.h"
-
+#include <QSettings>
 /**
  * @class Stgs
  *
  * Detailed description.
  */
+#define VER_GROUP_NAME QString("v%1").arg (version_)
 
 /* ------------------------------------------------------------------------- */
 /**
  * Detailed description for constructor.
  */
-Stgs::Stgs()
+Stgs::Stgs() :
+    version_(-1),
+    array_index_(-1),
+    impl_ (new QSettings())
 {
     STGS_TRACE_ENTRY;
+
+    impl_->beginGroup (VER_GROUP_NAME);
 
     STGS_TRACE_EXIT;
 }
@@ -36,6 +42,245 @@ Stgs::~Stgs()
 {
     STGS_TRACE_ENTRY;
 
+    impl_->endGroup();
+    impl_->sync ();
+    delete impl_;
     STGS_TRACE_EXIT;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+void Stgs::setVersion (int version)
+{
+    QStringList path;
+    for (;;) {
+        QString grp = impl_->group();
+        if (grp.isEmpty())
+            break;
+        path.append (grp);
+        impl_->endGroup();
+    }
+    version_ = version;
+
+    if (path.isEmpty()) {
+        path.append (VER_GROUP_NAME);
+    } else {
+        path[0] = VER_GROUP_NAME;
+    }
+
+    foreach(const QString & s, path) {
+        impl_->beginGroup (s);
+    }
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::beginGroup (const PERST_STRING & name)
+{
+    array_index_ = -1;
+    impl_->beginGroup (name);
+    return true;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::endGroup (const PERST_STRING & name)
+{
+    array_index_ = -1;
+    impl_->endGroup();
+    return true;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::beginWriteArray (const PERST_STRING & name, int predicted_count)
+{
+    array_index_ = -1;
+    impl_->beginWriteArray (name, predicted_count);
+    return true;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+int Stgs::beginReadArray (const PERST_STRING & name)
+{
+    array_index_ = -1;
+    return impl_->beginReadArray (name);
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::endArray (const PERST_STRING & name)
+{
+    impl_->endArray ();
+    array_index_ = -1;
+    return true;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+int Stgs::arrayIndex () const
+{
+    return array_index_;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::setArrayIndex (int index)
+{
+    impl_->setArrayIndex (index);
+    array_index_ = index;
+    return true;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+PERST_STRING Stgs::group () const
+{
+    return impl_->group ();
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+PERST_SLIST Stgs::groupPath () const
+{
+    QStringList path;
+    for (;;) {
+        QString grp = impl_->group();
+        if (grp.isEmpty())
+            break;
+        path.append (grp);
+        impl_->endGroup();
+    }
+    foreach(const QString & s, path) {
+        impl_->beginGroup (s);
+    }
+    return path;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool  Stgs::hasKey (const PERST_STRING & name)
+{
+    return impl_->contains (name);
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool  Stgs::hasKey (const PERST_SLIST & name)
+{
+    QString path = name.join (QLatin1String ("/"));
+    return impl_->contains (path);
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+PERST_STRING  Stgs::valueS (const PERST_STRING & name)
+{
+    return impl_->value(name).toString ();
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+PERST_SLIST  Stgs::valueSList (const PERST_STRING & name)
+{
+    return impl_->value(name).toStringList ();
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+int64_t Stgs::valueInt (const PERST_STRING & name)
+{
+    return impl_->value(name).toInt ();
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+uint64_t Stgs::valueUInt (const PERST_STRING & name)
+{
+    return impl_->value(name).toUInt ();
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+double Stgs::valueDbl (const PERST_STRING & name)
+{
+    return impl_->value(name).toReal ();
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool  Stgs::setValue (const PERST_STRING & name, const PERST_STRING & value)
+{
+    impl_->setValue (name, value);
+    return true;
+}
+/* ========================================================================= */
+
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::setValue (const PERST_STRING & name, const PERST_SLIST & value)
+{
+    impl_->setValue (name, value);
+    return true;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::setValue (const PERST_STRING & name, int64_t value)
+{
+    impl_->setValue (name, value);
+    return true;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::setValue (const PERST_STRING & name, uint64_t value)
+{
+    impl_->setValue (name, value);
+    return true;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::setValue (const PERST_STRING & name, double value)
+{
+    impl_->setValue (name, value);
+    return true;
+}
+/* ========================================================================= */
+
+
+
+// not part of the PerstInterface
+
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::setValue (const PERST_STRING & name, const QByteArray & value)
+{
+    impl_->setValue (name, value);
+    return true;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+QByteArray Stgs::valueBA (const PERST_STRING & name)
+{
+    return impl_->value (name).toByteArray ();
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::setValue (const PERST_STRING & name, bool value)
+{
+    impl_->setValue (name, value);
+    return true;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+bool Stgs::valueB (const PERST_STRING & name)
+{
+    return impl_->value (name).toBool ();
 }
 /* ========================================================================= */
--- stgs.h
+++ stgs.h
@@ -10,10 +10,22 @@
 #ifndef GUARD_STGS_H_INCLUDE
 #define GUARD_STGS_H_INCLUDE
 
+#undef PERST_STRING
+#define PERST_STRING QString
+
 #include <stgs/stgs-config.h>
+#include <perst/perst_interface.h>
+
+QT_BEGIN_NAMESPACE
+class QSettings;
+QT_END_NAMESPACE
 
 //! brief description
-class STGS_EXPORT Stgs {
+class STGS_EXPORT Stgs : public PerstInterface {
+
+    int version_; /**< a number identifying the version */
+    int array_index_; /**< index in current array */
+    QSettings * impl_;
 
 public:
 
@@ -22,6 +34,230 @@ public:
 
     //! Destructor.
     virtual ~Stgs();
+
+    //! Select current version.
+    void
+    setVersion (
+            int version);
+
+    //! Tell current version.
+    int
+    version () const {
+        return version_;
+    }
+
+
+    //! Starts a group; internal state is altered.
+    virtual bool
+    beginGroup (
+            const PERST_STRING & name);
+
+    //! Ends a group; internal state is altered.
+    virtual bool
+    endGroup (
+            const PERST_STRING & name = "");
+
+    //! Starts writing an array; internal state is altered.
+    virtual bool
+    beginWriteArray (
+            const PERST_STRING & name,
+            int predicted_count = -1);
+
+    //! Starts reading an array; internal state is altered.
+    virtual int
+    beginReadArray (
+            const PERST_STRING & name);
+
+    //! Ends an array; internal state is altered.
+    virtual bool
+    endArray (
+            const PERST_STRING & name = "");
+
+    //! Get current index of an array.
+    virtual int
+    arrayIndex () const;
+
+    //! Set current index of an array.
+    virtual bool
+    setArrayIndex (
+            int index);
+
+    //! Tell the name of current group (arrays are also groups).
+    virtual PERST_STRING
+    group () const;
+
+    //! Tell the full path of current group (arrays are also groups).
+    virtual PERST_SLIST
+    groupPath () const;
+
+    //! Tell if a given key exists (relative to current group).
+    virtual bool
+    hasKey (
+            const PERST_STRING & name);
+
+    //! Tell if a given path exists (relative to current group).
+    virtual bool
+    hasKey (
+            const PERST_SLIST & name);
+
+
+    //! Get the string value for a key; the value type MUST be string.
+    virtual PERST_STRING
+    valueS (
+            const PERST_STRING & name);
+
+    //! Get the string list for a key; the value type MUST be an array of strings.
+    virtual PERST_SLIST
+    valueSList (
+            const PERST_STRING & name);
+
+    //! Get the integer value for a key; the value type MUST be integer.
+    virtual int64_t
+    valueInt (
+            const PERST_STRING & name);
+
+    //! Get the integer value for a key; the value type MUST be integer.
+    virtual uint64_t
+    valueUInt (
+            const PERST_STRING & name);
+
+    //! Get the real value for a key; the value type MUST be real.
+    virtual double
+    valueDbl (
+            const PERST_STRING & name);
+
+
+    //! Get the string value for a key; the value type MUST be string.
+    inline PERST_STRING
+    valueS (
+            const PERST_STRING & name,
+            const PERST_STRING & default_val) {
+        if (hasKey (name)) return valueS (name);
+        else return default_val;
+    }
+
+    //! Get the string list for a key; the value type MUST be an array of strings.
+    inline PERST_SLIST
+    valueSList (
+            const PERST_STRING & name,
+            const PERST_SLIST & default_val) {
+        if (hasKey (name)) return valueSList (name);
+        else return default_val;
+    }
+
+    //! Get the integer value for a key; the value type MUST be integer.
+    inline int64_t
+    valueInt (
+            const PERST_STRING & name,
+            int64_t default_val) {
+        if (hasKey (name)) return valueInt (name);
+        else return default_val;
+    }
+
+    //! Get the integer value for a key; the value type MUST be integer.
+    inline uint64_t
+    valueUInt (
+            const PERST_STRING & name,
+            uint64_t default_val) {
+        if (hasKey (name)) return valueUInt (name);
+        else return default_val;
+    }
+
+    //! Get the real value for a key; the value type MUST be real.
+    inline double
+    valueDbl (
+            const PERST_STRING & name,
+            double default_val) {
+        if (hasKey (name)) return valueDbl (name);
+        else return default_val;
+    }
+
+
+    //! Set the string value for a key.
+    virtual bool
+    setValue (
+            const PERST_STRING & name,
+            const PERST_STRING & value);
+
+    //! Set the string list for a key.
+    virtual bool
+    setValue (
+            const PERST_STRING & name,
+            const PERST_SLIST & value);
+
+    //! Set the integer value for a key.
+    virtual bool
+    setValue (
+            const PERST_STRING & name,
+            int64_t value);
+
+    //! Set the integer value for a key.
+    virtual bool
+    setValue (
+            const PERST_STRING & name,
+            uint64_t value);
+
+    //! Set the integer value for a key.
+    virtual bool
+    setValue (
+            const PERST_STRING & name,
+            int value) {
+        return setValue (name, (int64_t)value);
+    }
+
+    //! Set the real value for a key.
+    virtual bool
+    setValue (
+            const PERST_STRING & name,
+            double value);
+
+
+
+
+
+    // not part of the PerstInterface
+
+
+
+    //! Get the real value for a key; the value type MUST be real.
+    inline QByteArray
+    valueBA (
+            const PERST_STRING & name,
+            const QByteArray & default_val) {
+        if (hasKey (name)) return valueBA (name);
+        else return default_val;
+    }
+
+    QByteArray
+    valueBA (
+            const PERST_STRING & name);
+
+    //! Set the real value for a key.
+    virtual bool
+    setValue (
+            const PERST_STRING & name,
+            const QByteArray & value);
+
+
+    //! Get the real value for a key; the value type MUST be real.
+    inline bool
+    valueB (
+            const PERST_STRING & name,
+            bool default_val) {
+        if (hasKey (name)) return valueB (name);
+        else return default_val;
+    }
+
+    bool
+    valueB (
+            const PERST_STRING & name);
+
+    //! Set the real value for a key.
+    virtual bool
+    setValue (
+            const PERST_STRING & name,
+            bool value);
+
 
 protected:
 
